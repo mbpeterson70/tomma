d7a710dd4ddc2397c094a272f8e3b11995331cee
Mason Peterson <mbpeterson70@gmail.com>
2023-05-06 15:25:26 -0400

    clean up code
    Signed-off-by: Mason Peterson <mbpeterson70@gmail.com>

--- src/path_planner.py
+++ src/path_planner.py
@@ -24,18 +24,7 @@ class PathPlanner():
         
         dt = tf/self.N
         for k in range(self.N):
-            # xp1 = self.dynamics.propagate(x[:,k], u[:,k],  dt)
             opti.subject_to(x[:,k+1] == self.dynamics.propagate(x[:,k], u[:,k],  dt))
-            # for i in range(self.dynamics.x_shape):
-            #     # print(xp1[i,k+1])
-            #     # print()
-            #     # print(x[i, k+1])
-            #     # print(xp1.item(i))
-            #     opti.subject_to(x[i,k+1] == xp1.item(i))
-            # exit(0)
-            
-        # x0_param = opti.param(self.dynamics.x_shape)
-        # xf_param = opti.param(self.dynamics.x_shape)
         
         for i in range(self.dynamics.x_shape):
             opti.subject_to(x[i,0] == x0.item(i))
@@ -48,34 +37,5 @@ class PathPlanner():
                 
         opti.solver('ipopt', self.solver_opts)
 
-        try:
-            sol = opti.solve()
-            import matplotlib.pyplot as plt
-            fig, ax = plt.subplots()
-            ax.plot(np.arange(x.shape[1]), sol.value(x)[0,:])
-            ax.plot(np.arange(x.shape[1]), sol.value(x)[1,:])
-            ax.plot(np.arange(x.shape[1]), sol.value(x)[2,:])
-
-            fig, ax = plt.subplots()
-            ax.plot(np.arange(u.shape[1]), sol.value(u)[0,:])
-            ax.plot(np.arange(u.shape[1]), sol.value(u)[1,:])
-            
-            plt.show()
-        except:
-            # import matplotlib.pyplot as plt
-            # # import ipdb; ipdb.set_trace()
-            # fig, ax = plt.subplots()
-            # ax.plot(np.arange(x.shape[1]), opti.debug.value(x)[0,:])
-            # ax.plot(np.arange(x.shape[1]), opti.debug.value(x)[1,:])
-            # ax.plot(np.arange(x.shape[1]), opti.debug.value(x)[2,:])
-
-            # fig, ax = plt.subplots()
-            # ax.plot(np.arange(u.shape[1]), opti.debug.value(u)[0,:])
-            # ax.plot(np.arange(u.shape[1]), opti.debug.value(u)[1,:])
-            
-            # plt.show()
-
-        # import ipdb; ipdb.set_trace()
-            print('faillll')
-        
-        return 
+        sol = opti.solve()
+        return sol.value(x), sol.value(u), sol.value(tf)
--- src/rover_playground.py
+++ src/rover_playground.py
@@ -1,19 +1,29 @@
 import numpy as np
+import matplotlib.pyplot as plt
 
 from path_planner import PathPlanner
 from dubins_dynamics import DubinsDynamics
 
 dubins = DubinsDynamics()
 planner = PathPlanner(dubins, 100)
+deg2rad = np.pi/180
 x0 = np.array([0.0, 0.0, 0.0])
-xf = np.array([2.0, 2*np.pi/3, 0.0])
-# u_bounds = np.array([
-#     [-1., 1.],
-#     [-.5, .5]
-# ])
+xf = np.array([2.0, 0.0, 2*np.pi/3])
 u_bounds = np.array([
     [-1, 1.],
-    [-.5, .5]
+    [-45*deg2rad, 45*deg2rad]
 ])
-# planner.solve_min_time(x0, xf)
-planner.solve_min_time(x0, xf, u_bounds)
+x, u, tf = planner.solve_min_time(x0, xf, u_bounds)
+fig, ax = plt.subplots()
+ax.plot(np.arange(x.shape[1])*tf/x.shape[1], x[0,:])
+ax.plot(np.arange(x.shape[1])*tf/x.shape[1], x[1,:])
+ax.plot(np.arange(x.shape[1])*tf/x.shape[1], x[2,:])
+
+fig, ax = plt.subplots()
+ax.plot(np.arange(u.shape[1])*tf/u.shape[1], u[0,:])
+ax.plot(np.arange(u.shape[1])*tf/u.shape[1], u[1,:])
+
+fig, ax = plt.subplots()
+ax.plot(x[0,:], x[1,:])
+
+plt.show()
